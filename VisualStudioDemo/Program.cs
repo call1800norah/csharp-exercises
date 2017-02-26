512d282eae1409d0d8e24b7d2277939ff01b6574
Norah Lovelace <call1800norah@gmail.com>
2017-02-26 15:28:41 -0600

    Add message

---
+++ VisualStudioDemo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VisualStudioDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Visual Studio Demo");
+            Console.ReadLine();
+                
+        }
+    }
+}
