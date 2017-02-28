7763619d0fc1c965cbe47d1ce6c44eec1a3466c3
Norah Lovelace <call1800norah@gmail.com>
2017-02-28 15:58:19 -0600

    studio mission AreaOfCircle is done.

---
+++ AreaOfCircle/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AreaOfCircle
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            double radius;
+            double area;
+            double number;
+            int tryAgain = 0;
+            bool negaNum = true;
+            do
+            {
+                Console.Write("Please enter the number: ");
+                string input = Console.ReadLine();
+                number = double.Parse(input);
+                area = Math.PI * number * number;
+                tryAgain++;
+                if (number > 0)
+                {
+                    negaNum = false;
+                    Console.WriteLine(area);
+
+                }
+                else
+                {
+                    Console.WriteLine("Please enter the positive number!");
+                }
+
+
+            } while (negaNum);
+             Console.ReadLine();
+        }
+    }
+}
